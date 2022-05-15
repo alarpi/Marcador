f566494d8ba8a41a697c64cbabb3dc7fed11e989
Javier <yard.campbs@gmail.com>
2022-05-15 22:32:04 +0100

    Change JSON name

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 let leftSide = Array.from(document.getElementsByClassName("left-side"));
 
+let teams = JSON.parse()
+
 leftSide.forEach(e => {
     e.addEventListener("click", x => {
         alert("¡Hola!, soy de color: " + e.parentElement.getAttribute("data-value"));
