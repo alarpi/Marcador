a828e549f7d6abf6c1550d6a493d3c23322a638a
Javier <yard.campbs@gmail.com>
2022-05-16 13:42:45 +0100

    Storage points of teams on local

--- js/main.js
+++ js/main.js
@@ -1,28 +1,77 @@
 let leftSide = Array.from(document.getElementsByClassName("left-side"));
 let rightSide = Array.from(document.getElementsByClassName("right-side"));
 let scores = Array.from(document.getElementsByClassName("score"));
-let teams = [];
+let timer = document.getElementById("timer");
+let jsonScores = {
+  teams: [
+    {
+      color: "red",
+      score: 0,
+    },
+    {
+      color: "yellow",
+      score: 0,
+    },
+    {
+      color: "blue",
+      score: 0,
+    },
+    {
+      color: "purple",
+      score: 0,
+    },
+    {
+      color: "green",
+      score: 0,
+    },
+    {
+      color: "orange",
+      score: 0,
+    },
+  ],
+};
 
-fetch("data.json")
-  .then((res) => res.json())
-  .then((data) => {
-    data.teams.forEach((e, i) => {
-      teams.push(e);
-    });
+function updateJSON(currentLocalStorage, currentIteration, operator) {
+  let currentData = JSON.parse(currentLocalStorage);
+  if (operator === "+") {
+    currentData.teams[currentIteration].score++;
+  } else {
+    if (currentData.teams[currentIteration].score != 0) {
+      currentData.teams[currentIteration].score--;
+    }
+  }
+  localStorage.setItem("teams", JSON.stringify(currentData));
+}
+
+window.onload = () => {
+  if (!localStorage.getItem("timer")) {
+    localStorage.setItem("timer", "20:00:00");
+  }
+  if (!localStorage.getItem("teams")) {
+    localStorage.setItem("teams", JSON.stringify(jsonScores));
+  }
+  scores.forEach((e, i) => {
+    e.innerText = JSON.parse(localStorage.getItem("teams")).teams[i].score;
   });
-
-console.log(scores[0]);
-
-leftSide.forEach((e, i) => {
-  e.addEventListener("click", (x) => {
-    scores[i].innerText++;
-  });
-});
+};
 
 rightSide.forEach((e, i) => {
   e.addEventListener("click", (x) => {
-    if(scores[i].innerText != 0){
-      scores[i].innerText--;
-    }
+    updateJSON(localStorage.getItem("teams"), i, "+");
+    scores[i].innerText = JSON.parse(localStorage.getItem("teams")).teams[
+      i
+    ].score;
   });
 });
+
+leftSide.forEach((e, i) => {
+  e.addEventListener("click", (x) => {
+    updateJSON(localStorage.getItem("teams"), i, "-");
+    scores[i].innerText = JSON.parse(localStorage.getItem("teams")).teams[
+      i
+    ].score;
+  });
+});
+
+
+
