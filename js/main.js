b2a0ab4e40a299aa0306544f473bdf9bda27543f
Javier <yard.campbs@gmail.com>
2022-05-16 10:36:55 +0100

    Increase and decrease scores of teams

--- js/main.js
+++ js/main.js
@@ -1,18 +1,26 @@
 let leftSide = Array.from(document.getElementsByClassName("left-side"));
-let scores = document.getElementsByClassName("score");
+let rightSide = Array.from(document.getElementsByClassName("right-side"));
+let scores = Array.from(document.getElementsByClassName("score"));
+let teams = [];
 
 fetch("data.json")
   .then((res) => res.json())
   .then((data) => {
     data.teams.forEach((e, i) => {
-      scores[i].innerText = e.score;
+      teams.push(e);
     });
   });
 
-leftSide.forEach((e) => {
+console.log(scores[0]);
+
+leftSide.forEach((e, i) => {
   e.addEventListener("click", (x) => {
-    alert(
-      "¡Hola!, soy de color: " + e.parentElement.getAttribute("data-value")
-    );
+    scores[i].innerText++;
+  });
+});
+
+rightSide.forEach((e, i) => {
+  e.addEventListener("click", (x) => {
+    scores[i].innerText--;
   });
 });
